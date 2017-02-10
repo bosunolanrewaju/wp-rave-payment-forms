6623dc7382ad2aec87887b5d585289b82d5b3dd5
bosunolanrewaju <bosunolanrewaju@gmail.com>
2017-02-10 20:15:15 +0100

    Add go live option

--- includes/rave-base-class.php
+++ includes/rave-base-class.php
@@ -15,6 +15,7 @@
     class FLW_Rave_Pay {
 
       private $plugin_name = 'rave-payment-forms';
+      private $api_base_url = '//flw-pms-dev.eu-west-1.elasticbeanstalk.com/';
 
       /**
        * Instance variable
@@ -74,6 +75,9 @@
           FLW_Tinymce_Plugin::get_instance();
         }
 
+        if ($admin_settings->get_option_value( 'go_live' ) === 'yes' ) {
+          $this->api_base_url = '//api.ravepay.co/';
+        }
 
       }
 
@@ -85,11 +89,13 @@
       public function admin_notices() {
 
         $options = get_option( 'flw_rave_options' );
+        $no_public_key = (bool) (! array_key_exists('public_key', $options ) || empty( $options['public_key'] ));
+        $no_secret_key = (bool) (! array_key_exists('secret_key', $options ) || empty( $options['secret_key'] ));
 
-        if ( ! array_key_exists('public_key', $options ) || empty( $options['public_key'] ) ) {
+        if ( $no_secret_key || $no_public_key ) {
           echo '<div class="updated"><p>';
           echo  __( 'Rave payment form plugin is installed. - ', 'rave-pay' );
-          echo "<a href=" . esc_url( add_query_arg( 'page', $this->plugin_name, admin_url( 'admin.php' ) ) ) . " class='button-primary'>" . __( 'Enter your Rave "Pay Button" Public Key to start accepting payments', 'rave' ) . "</a>";
+          echo "<a href=" . esc_url( add_query_arg( 'page', $this->plugin_name, admin_url( 'admin.php' ) ) ) . " class='button-primary'>" . __( 'Enter your Rave "Pay Checkout" Public Key and Secret Key to start accepting payments', 'rave' ) . "</a>";
           echo '</p></div>';
         }
 
@@ -106,34 +112,42 @@
 
         check_ajax_referer( 'flw-rave-pay-nonce', 'flw_sec_code' );
 
-        $response_code = ( $_POST['paymentType'] === 'account' ) ? $_POST['acctvalrespcode'] : $_POST['vbvrespcode'];
-        $status =  ( $response_code == '00' ) ? 'successful' : 'failed';
-        $args   =  array(
-          'post_type'   => 'payment_list',
-          'post_status' => 'publish',
-          'post_title'       => $_POST['txRef'],
-        );
+        $tx_ref = $_POST['txRef'];
+        $secret_key = $admin_settings->get_option_value( 'secret_key' );
 
-        $payment_record_id = wp_insert_post( $args, true );
-
-        if ( ! is_wp_error( $payment_record_id )) {
-
-          $post_meta = array(
-            '_flw_rave_payment_amount'   => $_POST['amount'],
-            '_flw_rave_payment_customer' => $_POST['customer']['email'],
-            '_flw_rave_payment_status'   => $status,
-            '_flw_rave_payment_tx_ref'   => $_POST['txRef'],
+        $txn = json_decode( $this->_fetchTransaction($tx_ref, $secret_key) );
+        if ( ! empty($txn->data) && $txn->data->status === 'successful' ) {
+          $status =  $txn->data->status;
+          $args   =  array(
+            'post_type'   => 'payment_list',
+            'post_status' => 'publish',
+            'post_title'  => $tx_ref,
           );
 
-          $this->add_post_meta( $payment_record_id, $post_meta );
+          $payment_record_id = wp_insert_post( $args, true );
 
+          if ( ! is_wp_error( $payment_record_id )) {
+
+            $post_meta = array(
+              '_flw_rave_payment_amount'   => $txn->data->amount,
+              '_flw_rave_payment_customer' => $_POST['customer']['email'],
+              '_flw_rave_payment_status'   => $status,
+              '_flw_rave_payment_tx_ref'   => $tx_ref,
+            );
+
+            $this->_add_post_meta( $payment_record_id, $post_meta );
+
+          }
         }
-
         $redirect_url_key = $status === 'successful' ? 'success_redirect_url' : 'failed_redirect_url';
 
         echo json_encode( array( 'status' => $status, 'redirect_url' => $admin_settings->get_option_value( $redirect_url_key ) ) );
         die();
 
+      }
+
+      public function get_api_base_url() {
+        return $this->api_base_url;
       }
 
       public static function gen_rand_string($len=4) {
@@ -143,13 +157,23 @@
         return bin2hex( openssl_random_pseudo_bytes( $len/2 ) );
       }
 
+      private function _fetchTransaction( $tx_ref, $secret_key ) {
+        $url = "http:" . $this->get_api_base_url() . "/tx/verify?tx_ref=$tx_ref&seckey=$secret_key";
+        $response = wp_remote_get( $url );
+        $result = wp_remote_retrieve_response_code( $response );
+        if( $result === 200 ){
+          return wp_remote_retrieve_body( $response );
+        }
+        return [];
+      }
+
       /**
        * Adds metadata to payment list post type
        *
        * @param [int]   $post_id  The ID of the post to add metadata to
        * @param [array] $data     Collection of the data to be added to the post
        */
-      private function add_post_meta( $post_id, $data ) {
+      private function _add_post_meta( $post_id, $data ) {
 
         foreach ($data as $meta_key => $meta_value) {
           update_post_meta( $post_id, $meta_key, $meta_value );
